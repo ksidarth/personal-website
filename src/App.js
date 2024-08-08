31d22aea137894f8c9e366b23be91b48a31e1a86
sp-skulkarni <ksidarth16@gmail.com>
2024-08-08 11:00:52 -0700

    vercel committ

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
-import Blog from './components/blog';
+import Blog from '../src/components/blog';
 
 
 function App() {
